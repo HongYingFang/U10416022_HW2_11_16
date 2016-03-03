afd5294f891b6a335d45e2f7585aa52a86e94251
HongYingFang <zx04785@gmail.com>
2016-03-03 23:22:26 +0800

    Create TestRepeatAdditionQuiz.java
    it is test program for RepeatAdditionQuiz.java

---
+++ TestRepeatAdditionQuiz.java
@@ -0,0 +1,12 @@
+//U10416022洪映舫
+import java.util.Scanner;
+import java.util.ArrayList;
+public class TestRepeatAdditionQuiz{
+  public static void main(String[] args){
+	
+    int number1 = (int)(Math.random() * 10);
+    int number2 = (int)(Math.random() * 10);
+		RepeatAdditionQuiz quiz = new RepeatAdditionQuiz(number1,number2);		
+	
+	}
+}
