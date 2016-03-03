d240b330bde16cd9b1580524c693dcbb9d3bc5dd
HongYingFang <zx04785@gmail.com>
2016-03-03 23:15:43 +0800

    Update RepeatAdditionQuiz.java
    divide the program to two class that include TestRepeatAdditionQuiz.java and RepeatAdditionQuiz.java

--- RepeatAdditionQuiz.java
+++ RepeatAdditionQuiz.java
@@ -4,26 +4,30 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class RepeatAdditionQuiz{
-	public static void main(String[] args){
 
-		//set two numbers at random
-		int number1 = (int)(Math.random() * 10);
-		int number2 = (int)(Math.random() * 10);
+	//set two numbers at random
+	int number1 = (int)(Math.random() * 10);
+	int number2 = (int)(Math.random() * 10);
 
-		// Create the Scanner
-		Scanner input = new Scanner(System.in);
-		 
+	RepeatAdditionQuiz(int number1,int number2){
 
-		//print out the question for user
-		System.out.print("What is " + number1 + " * " + number2 + "? ");
-		//user inputs their answer
-		int answer = input.nextInt();
 
 		//set up arraylist method for integer
 		ArrayList<Integer> doneAnswers = new ArrayList<>();
 
+		//print out the question for user
+		System.out.print("What is " + number1 + " * " + number2 + "? ");
+
+		// Create the Scanner
+		Scanner input = new Scanner(System.in);
+
+		
+		//user inputs their answer
+		int answer = input.nextInt();
+
+		
 		//use while loop and give the condition that user input wrong answer    
-		while (number1 * number2 != answer) {
+		while (number1 * number2 != answer) 
 
 			//print out the answers that users has already inputted
 			if(doneAnswers.contains(answer)){
@@ -38,8 +42,11 @@ public class RepeatAdditionQuiz{
 			doneAnswers.add(answer);
 			
 		}
-
 		//print out the message that the user inputs correct answer 
-		System.out.println("You got it!");
+		System.out.println("You got it!");	
 	}
+	
 }
+
+
+
